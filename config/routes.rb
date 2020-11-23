df47ff63073db3b30c477d359ce9375a35f9eb16
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-11-23 17:50:33 +0800

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :caterings
+      resources :caterings do
+        resources :reservations
+      end
+      resources :reservations do
+        resources :reviews
+      end
     end
   end
+  post '/login', to: 'login#login'
 end
