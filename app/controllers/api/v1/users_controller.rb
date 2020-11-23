215a7a72653068d6d5d3e9a420bd75c73043a8d7
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-11-23 22:33:33 +0800

    added users

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < Api::V1::BaseController
+  def index
+    @users = User.all
+    render json: @reviews #Just for testing
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @review #Just for testing
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:open_id, :name, :phonenumber)
+  end
+end
