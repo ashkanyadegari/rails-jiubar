7dc007e64a75684911efb7a3dd8d1c39a811839a
Wenqi Yu <kikiyu1006@gmail.com>
2020-11-26 12:54:09 +0800

    search

--- app/models/catering.rb
+++ app/models/catering.rb
@@ -4,4 +4,11 @@ class Catering < ApplicationRecord
   has_many :items
   has_many :reservations
   has_many :reviews, through: :reservations
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
